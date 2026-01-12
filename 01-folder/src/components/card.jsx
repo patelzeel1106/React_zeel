e1b908b86cf5cf7385d4a4f1f60e8ef66027300c
patelzeel1106 <127917382+patelzeel1106@users.noreply.github.com>
2026-01-12 12:43:40 +0530

    0-1 folder done

---
+++ 01-folder/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+      <div className='card'>
+        <h1>Zeel Patel</h1>
+        <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, praesentium.</h2>
+    </div>
+  )
+}
+
+export default card
