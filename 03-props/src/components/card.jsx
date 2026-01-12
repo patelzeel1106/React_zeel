2d18aba1b9b15ffb3d885a817a814c3d854a3b63
patelzeel1106 <127917382+patelzeel1106@users.noreply.github.com>
2026-01-12 16:13:22 +0530

    03-props done

---
+++ 03-props/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+       <img src="./src/image/winter-9988327_1280.jpg" alt="" />
+      <h1>Zeel patel</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <button>View Profile</button>
+    </div>
+  )
+}
+
+export default Card
